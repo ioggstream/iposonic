e610c485843f1741ad277d25f10824cb401baa0f
Roberto Polli <robipolli@gmail.com>
2013-03-13 23:53:46 +0100

    minor fixes to testrunner

--- runtest.py
+++ runtest.py
@@ -4,11 +4,16 @@ from os import system
 
 
 def main(argc, argv):
-    if argc:
-        test_class, test_case = argv[1].split(".", 1)
-        argv.pop(1)
-    system("nosetests test/%s.py:%s %s" % (test_class, test_case,
-           " ".join(argv)))
+    try:
+        params = [ x for x in argv if x.startswith('-') ]
+        suite = [ x for x in argv if not x.startswith('-') ]
+        
+        test_class, test_case = suite[1].split(".", 1)
+        return system("nosetests test/%s.py:%s %s" % (test_class, test_case,
+           " ".join(params)))
+    except:
+        print("usage: %s [params] testsuite.class.case ")
+        return 1
 
 if __name__ == '__main__':
     (argc, argv) = (len(sys.argv), sys.argv)
